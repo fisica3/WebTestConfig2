de6aa82fc773705fa01c1fa62c46f399375f1ab9
Ernesto Cárdenas Cangahuala <ecardenas@rocketmail.com>
2016-01-21 12:03:27 -0500

    Subiendo al Repo Local

---
+++ src/WebTestConfig2/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebTestConfig2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfigurationRoot _config;
+        public HomeController(IConfigurationRoot config)
+        {
+            this._config = config;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            var DondeEstoy = HelperConfig.GetAppSettings(_config, "DondeEstoy");
+            if (String.IsNullOrEmpty(DondeEstoy))
+                ViewBag.Message = "No se pudo recuperar";
+            else ViewBag.Message = "Nuestro entorno con DI y Helper es " + DondeEstoy;
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/WebTestConfig2/Helper.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebTestConfig2
+{
+    public enum ConnPrefix
+    {
+        SqlDatabase,
+        SqlServer,
+        MySql,
+        Custom,
+        Empty
+    }
+
+    public static class EnumUtil
+    {
+        public static IEnumerable<T> GetValues<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+    }
+    static public class HelperConfig
+    {
+        static string GetConnPrefixDescritpion(ConnPrefix prefix)
+        {
+            switch (prefix)
+            {
+                case ConnPrefix.SqlDatabase:
+                    return "SQLAZURECONNSTR_";                  
+                case ConnPrefix.SqlServer:
+                    return "SQLCONNSTR_";
+                case ConnPrefix.MySql:
+                    return "MYSQLCONNSTR_";
+                case ConnPrefix.Custom:
+                    return "CUSTOMCONNSTR_";
+                case ConnPrefix.Empty:
+                    return string.Empty;  
+            }
+            return string.Empty;
+        }
+        public static string GetAppSettings(IConfigurationRoot config, string key)
+        {
+            var localKey = "AppSettings:" + key;
+            var envKey = "APPSETTING_" +key;
+            return config[envKey] ?? config[localKey];
+        }
+
+
+        public static string GetConnectionString(IConfigurationRoot config, string key, ConnPrefix prefix = ConnPrefix.Empty)
+        {
+            string localKey = "Data:" + key + ":ConnectionString";
+            if (prefix == ConnPrefix.Empty)
+            {
+                var values = EnumUtil.GetValues<ConnPrefix>();
+
+                foreach (var item in values)
+                {
+                   string tryEnvKey = GetConnPrefixDescritpion(item) + key;
+                    if (config[tryEnvKey] != null)
+                        return config[tryEnvKey];
+                }
+                return config[localKey];
+            }
+            else
+            {
+                string envKey = GetConnPrefixDescritpion(prefix) + key;
+                return config[envKey] ?? config[localKey];
+            }
+        }
+    }
+
+
+    /*
+    Connection String Type  Environment Variable Prefix
+SQL Database  SQLAZURECONNSTR_
+SQL Server  SQLCONNSTR_
+MySQL  MYSQLCONNSTR_
+Custom  CUSTOMCONNSTR
+    */
+
+}
